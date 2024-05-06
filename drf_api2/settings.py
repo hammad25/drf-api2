6303212ebaf33560eed32dd1df15535b7e70b436
Hammad <hammad2091504@hotmail.com>
2024-05-06 11:31:54 +0000

    fix cloudinary

--- drf_api2/settings.py
+++ drf_api2/settings.py
@@ -15,9 +15,10 @@ import os
 
 if os.path.exists('env.py'):
     import env
-    CLOUDINARY_STORAGE = {
+
+CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
-    }
+}
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
