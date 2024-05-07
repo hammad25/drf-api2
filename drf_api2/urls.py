5272f85ea1da50080dba8fbd94cef3934827b87c
Hammad <hammad2091504@hotmail.com>
2024-05-07 12:47:49 +0000

    Add followers and refactor

--- drf_api2/urls.py
+++ drf_api2/urls.py
@@ -24,6 +24,9 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
+    path('', include('followers.urls')),
+
+
 
 
 
