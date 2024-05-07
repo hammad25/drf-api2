19f257ecb79082854c6a68749ce3939b754080a4
Hammad <hammad2091504@hotmail.com>
2024-05-07 12:56:51 +0000

    Add home page

--- drf_api2/urls.py
+++ drf_api2/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,7 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
+    path('', home, name='home'),
 
 
 
